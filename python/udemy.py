d6f3285d53bb751b6037408dcb17568bd793039d
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-05-14 16:25:03 +0300

    up

--- python/udemy.py
+++ python/udemy.py
@@ -7,21 +7,21 @@ def count_down(seconds):
         hours, minutes = divmod(minutes, 60)
 
         timer = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
-        print(timer, end="\r")  # Aynı satırda süreyi güncelleyerek göstermek için 'end="\r"' kullanılır
-        time.sleep(1)  # 1 saniye bekletme
+        print(timer, end="\r") 
+        time.sleep(1) 
 
         seconds -= 1
 
     print("Süre Bitti!")
 
 
-# Kullanıcıdan süreyi alınır
+
 hours = int(input("Saat: "))
 minutes = int(input("Dakika: "))
 seconds = int(input("Saniye: "))
 
-# Süreyi saniyeye çevirilir
+
 total_seconds = (hours * 3600) + (minutes * 60) + seconds
 
-# Sayma işlemi başlatılır
+
 count_down(total_seconds)
