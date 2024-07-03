83cf670fac31202bae0a6515418d456c382d7714
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-07-03 16:31:54 +0300

    add the project

---
+++ timer.py
@@ -0,0 +1,26 @@
+import time
+
+def count_down(seconds):
+    while seconds >= 0:
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+
+        timer = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+        print(timer, end="\r") 
+        time.sleep(1) 
+
+        seconds -= 1
+
+    print("Süre Bitti!")
+
+
+
+hours = int(input("Saat: "))
+minutes = int(input("Dakika: "))
+seconds = int(input("Saniye: "))
+
+
+total_seconds = (hours * 3600) + (minutes * 60) + seconds
+
+
+count_down(total_seconds)
